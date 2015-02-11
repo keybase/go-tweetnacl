2bba147a12807916a9e5ff1c5752f8fbfa10792c
Maxwell Krohn <themax@gmail.com>
2015-02-10 21:42:57 -0500

    compiling

--- c/lib/extras.c
+++ c/lib/extras.c
@@ -1,4 +1,6 @@
 
+#include "_cgo_export.h"
+
 int crypto_sign_keypair_seed(u8 *pk, u8 *sk, const u8 *seed) {
   u8 d[64];
   gf p[4];
@@ -15,4 +17,8 @@ int crypto_sign_keypair_seed(u8 *pk, u8 *sk, const u8 *seed) {
   FOR(i,32) sk[i] = seed[i];
   FOR(i,32) sk[32 + i] = pk[i];
   return 0;
+}
+
+void randombytes(u8 *buf, u64 n) {
+  RandomBytes(buf, n);
 }
---
+++ c/wrap.go
@@ -0,0 +1,32 @@
+
+package tweetnacl
+
+// #include "tweetnacl.h"
+import "C"
+
+import (
+	"unsafe"
+	"crypto/rand"
+	"log"
+)	
+
+type SignPublicKey [32]byte
+type SignSecretKey [64]byte
+type SignSeed [32]byte
+
+func CryptoSignKeypairSeed(pub SignPublicKey, sec SignSecretKey, seed SignSeed) {
+	C.crypto_sign_keypair_seed((*C.uchar)(unsafe.Pointer(&pub[0])),
+		(*C.uchar)(unsafe.Pointer(&sec[0])),
+		(*C.uchar)(unsafe.Pointer(&seed[0])))	
+}
+
+//export RandomBytes
+func RandomBytes(cbuf *C.uchar, n C.int) {
+	buf := C.GoBytes(unsafe.Pointer(cbuf), n)
+	if _, err := rand.Read(buf); err != nil {
+		log.Fatalf(err.Error())
+		return
+	}
+	return
+}
+
