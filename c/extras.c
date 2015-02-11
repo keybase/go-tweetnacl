c44667e956a98a0b24c823693733cdca8e943456
Maxwell Krohn <themax@gmail.com>
2015-02-10 20:32:16 -0500

    some playing around with the C lib

---
+++ c/extras.c
@@ -0,0 +1,18 @@
+
+int crypto_sign_keypair_seed(u8 *pk, u8 *sk, const u8 *seed) {
+  u8 d[64];
+  gf p[4];
+  int i;
+
+  crypto_hash(d, seed, 32);
+  d[0] &= 248;
+  d[31] &= 127;
+  d[31] |= 64;
+
+  scalarbase(p,d);
+  pack(pk,p);
+
+  FOR(i,32) sk[i] = seed[i];
+  FOR(i,32) sk[32 + i] = pk[i];
+  return 0;
+}
